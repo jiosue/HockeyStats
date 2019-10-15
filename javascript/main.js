538e54685df2bfe24f4129cdaa9b34540f2fbeff
jiosue <joe.iosue@yahoo.com>
2019-10-14 22:46:12 -0700

    added bindings

--- javascript/main.js
+++ javascript/main.js
@@ -28,7 +28,7 @@ function update_output() {
 		var stat = STATS_ORDER[i];
 		output += stat + ": " + STATS[stat].toString() + "<br/>";
 	}
-	output += "<br/>" + NOTES;
+	output += "<br/><span class='bold'>Notes</span>:<br/>" + NOTES;
 	document.getElementById("output").innerHTML = output;
 }
 
@@ -52,6 +52,7 @@ function start_shift() {
 	if (!on_ice) {
 		start_time = get_time();
 		on_ice = true;
+		document.getElementById("ice").innerHTML = 'on ice';
 	}
 }
 
@@ -60,9 +61,10 @@ function stop_shift() {
 	if (on_ice) {
 		STATS['TOI'] += get_time() - start_time;
 		increment_stat("Shifts");
-		STATS['TOI/shift'] = STATS['TOI'] / STATS['Shifts'];
+		STATS['TOI/shift'] = Math.round(STATS['TOI'] / STATS['Shifts']);
 		update_output();
 		on_ice = false;
+		document.getElementById("ice").innerHTML = 'off ice';
 	}
 }
 
@@ -72,7 +74,31 @@ function stop_shift() {
 document.onkeydown = function (e) {
 	e = e || window.event;
 	switch (e.which || e.keyCode) {
-		case 13 : // 13 is Enter
-			submit_note();
+		case 13 : // Submit note, Enter
+			submit_note(); break;
+		case 83 : // Start shift, s
+			start_shift(); break;
+		case 69 : // Stop shift, e
+			stop_shift(); break;
+		case 87 : // Faceoff win, w
+			if (!on_ice) {start_shift(); increment_stat('Faceoff Wins');} break;
+		case 76 : // Faceoff loss, l
+			if (!on_ice) {start_shift(); increment_stat('Faceoff Losses');} break;
+		case 71 : // Goal, g
+			increment_stat('Goals'); increment_stat('Plus'); stop_shift(); break;
+		case 65 : // Assist, a
+			increment_stat('Assists'); increment_stat('Plus'); stop_shift(); break;
+		case 80 : // Plus, p
+			increment_stat('Plus'); stop_shift(); break;
+		case 77 : // Minus, m
+			increment_stat('Minus'); stop_shift(); break;
+		case 88 : // Shot, x
+			increment_stat('Shots'); break;
+		case 66 : // Blocked shot, b
+			increment_stat('Blocked Shots'); break;
+		case 79 : // Offtarget shot, o
+			increment_stat('Off-target Shots'); break;
+		case 72 : // Hit, h
+			increment_stat('Hits'); break;
 	}
 }
